ea63405132f39c76cf174c1c0f0696f9a755b7b5
Chinmay <agarwalchinmay3007@gmail.com>
2024-10-06 12:55:29 +0530

    two way traffic

--- car.py
+++ car.py
@@ -1,15 +1,17 @@
 from turtle import Turtle
 import random
 
-back_speed = 5
+speed = 5
 starting_speed = 5
 
 class Car():
     
     def __init__(self):
-        self.cars = []
+        self.rcars = []
+        self.lcars = []
         self.reserve_car = []
         self.car_speed = starting_speed
+        self.x = 0
 
         
     def add_car(self):
@@ -24,8 +26,15 @@ class Car():
                 new_car.shapesize(stretch_len=2, stretch_wid=1) 
                 new_car.penup()
             
-            new_car.goto(300, random.randint(-230, 230))
-            self.cars.append(new_car)
+            if(self.x % 2 == 0):
+                new_car.goto(300, random.randint(-230, -10))
+                self.lcars.append(new_car)
+            else:
+                new_car.goto(-300, random.randint(10, 230))
+                self.rcars.append(new_car)
+            
+            self.x += 1
+
 
     def random_color(self):
         r = random.randint(0, 255)
@@ -33,13 +42,20 @@ class Car():
         b = random.randint(0, 255)
         return (r, g, b)
 
-    def move(self):
-        for c in self.cars:
+    def rmove(self):
+        for c in self.rcars:
+            c.goto(c.xcor() + self.car_speed, c.ycor())
+            if c.xcor() > 320:
+                self.rcars.remove(c)
+                self.reserve_car.append(c)
+
+    def lmove(self):
+        for c in self.lcars:
             c.goto(c.xcor() - self.car_speed, c.ycor())
             if c.xcor() < -320:
-                self.cars.remove(c)
+                self.lcars.remove(c)
                 self.reserve_car.append(c)
 
     def increase_back_movement(self):
-        self.car_speed += back_speed
+        self.car_speed += speed
 
--- main.py
+++ main.py
@@ -27,17 +27,32 @@ def move():
 
 move()
 
-
+middle_line = Turtle()
+middle_line.penup()
+middle_line.goto(300, 0)
+xi = 0
+while(middle_line.xcor() > -320):
+    if(xi % 2 == 0):
+        middle_line.pendown()
+    else:
+        middle_line.penup()
+    middle_line.backward(10)
+    xi += 1
 
 while game_is_on:
     time.sleep(0.1)
     screen.update()
     car.add_car()
-    car.move()
+    car.rmove()
+    car.lmove()
 
-    for c in car.cars:
-        if c.distance(player) < 15:
+    for c in car.lcars:
+        if c.distance(player) < 20:
             game_is_on = False
+    
+    for c in car.rcars:
+        if c.distance(player) < 20:
+            game_is_on = False    
 
     if (player.ycor() > 250):
         level.update_level()
